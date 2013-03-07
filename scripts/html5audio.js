26dcfae1cc3097996e515aed55f8879907af84e0
Tijmen van Eijsden <tijmen@Tijmens-MacBook-Pro.local>
2013-03-07 15:50:21 +0100

    Audio html5 example

--- scripts/html5audio.js
+++ scripts/html5audio.js
@@ -22,8 +22,51 @@ function onConfirmRetry(button) {
 function pad2(number) {
 	return (number < 10 ? '0' : '') + number
 }
+var stream = getUrlVars()["stream"];
 
-var myaudioURL = 'http://www.yourmuze.com/desktop/w-praise/mp3-128-s.m3u';
+switch (stream)
+{
+	case 'mp3-mp3':
+		var myaudioURL = 'http://http.yourmuze.com/twr-stream/mp3-128-s.mp3';
+		break;
+	case 'mp3-m3u':
+		var myaudioURL = 'http://www.yourmuze.com/desktop/twr-stream/mp3-128-s.m3u';
+		break;
+	case 'mp3-pls':
+		var myaudioURL = 'http://www.yourmuze.com/desktop/twr-stream/mp3-128-s.pls';
+		break;
+	case 'mp3-flv':
+		var myaudioURL = 'http://flash.yourmuze.com/twr-stream/mp3-128-s.flv';
+		break;
+	case 'aac-pls':
+		var myaudioURL = 'http://www.yourmuze.com/desktop/twr-stream/twrvooru.pls';
+		break;
+	case 'aac-sdp':
+		var myaudioURL = 'http://www.yourmuze.com/mobile/twr-stream/twrvooru.sdp';
+		break;
+	case 'aac-m3u':
+		var myaudioURL = 'http://www.yourmuze.com/desktop/twr-stream/twrvooru.m3u';
+		break;
+	case 'aac-http':
+		var myaudioURL = 'http://http.yourmuze.com/twr-stream/twrvooru.aac';
+		break;
+	case 'aac-rtsp':
+		var myaudioURL = 'rtsp://rtsp.yourmuze.com/twr-stream/twrvooru.aac';
+		break;
+	case 'aac-flv':
+		var myaudioURL = 'http://flash.yourmuze.com/twr-stream/twrvooru.flv';
+		break;
+	case 'vb-pls':
+		var myaudioURL = 'http://www.yourmuze.com/desktop/twr-stream/twrvooru-vorbis.pls';
+		break;
+	case 'vb-m3u':
+		var myaudioURL = 'http://www.yourmuze.com/desktop/twr-stream/twrvooru-vorbis.m3u';
+		break;
+	case 'vb-ogg':
+		var myaudioURL = 'http://http.yourmuze.com/twr-stream/twrvooru-vorbis.ogg';
+		break;
+}
+
 var myaudio = new Audio(myaudioURL);
 var isPlaying = false;
 var readyStateInterval = null;
@@ -100,3 +143,11 @@ var html5audio = {
 		textPosition.innerHTML = '';
 	}
 };
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
